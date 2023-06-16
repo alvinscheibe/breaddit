b421a30bb056f6854bd478d962256d40c20c77f2
Alvin Scheibe <alvin.scheibe@gmail.com>
2023-06-16 19:53:45 -0300

    Navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
 import '@/styles/globals.css'
+import React from 'react';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import Navbar from '@/components/navbar';
+
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
+
+const inter = Inter({
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
@@ -11,8 +20,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
+      <body className={'min-h-screen pt-12 bg-slate-50 antialiased'}>
+        <Navbar />
+
+        <div className={'container max-w-7xl mx-auto h-full pt-12'}>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
